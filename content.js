75a1d9433d7e37dec7804d567cde75bee59bfce8
screwtopkitten <72877287+screwtopkitten@users.noreply.github.com>
2021-04-26 12:15:04 +0100

    update

--- content.js
+++ content.js
@@ -5,12 +5,9 @@ console.log("Chrome Extension Ready to go?")
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse){
-    console.log(message.txt);
-    if(message.txt === "hello"){
-        let paragraphs = document.getElementsByTagName('p');
-        for(elt of paragraphs){
-            elt.style['background-color'] = '#8A2BE2';
-        }
-            
+    console.log(message);
+    let paragraphs = document.getElementsByTagName('p');
+    for (elt of paragraphs) {
+      elt.innerHTML = message.txt;
     }
 }
